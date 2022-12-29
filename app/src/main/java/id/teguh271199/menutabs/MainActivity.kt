76650bc2d880ada68862a95ee7325effd1fbdcb8
teguh271199 <mujiatmiimut91gmail.com>
2022-12-29 19:35:33 +0700

    V.0.1

---
+++ app/src/main/java/id/teguh271199/menutabs/MainActivity.kt
@@ -0,0 +1,164 @@
+package id.teguh271199.menutabs
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.google.accompanist.pager.*
+import id.teguh271199.menutabs.ui.theme.TabsTheme
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    @ExperimentalMaterialApi
+    @ExperimentalPagerApi
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+            MainScreen()
+
+        }
+    }
+}
+
+
+@ExperimentalMaterialApi
+@ExperimentalPagerApi
+@Composable
+fun MainScreen(){
+
+    val tabs = listOf(
+        TabItem.Home,
+        TabItem.Profile,
+        TabItem.Settings,
+    )
+
+    val pagerState = rememberPagerState(pageCount = tabs.size)
+
+    Scaffold(
+        topBar = { TopBar() }
+    ) {
+        Column() {
+            Tabs(tabs = tabs, pagerState = pagerState)
+            TabsContent(tabs = tabs, pagerState = pagerState)
+        }
+    }
+
+}
+
+@Composable
+fun TopBar(){
+
+    TopAppBar(
+        title = { Text(text = "Menu Tabs", fontSize = 18.sp) },
+        backgroundColor = Color.Cyan,
+        contentColor = Color.Black
+    )
+
+}
+
+@ExperimentalMaterialApi
+@ExperimentalPagerApi
+@Composable
+fun Tabs(tabs: List<TabItem>, pagerState: PagerState){
+
+    val scope = rememberCoroutineScope()
+    TabRow(
+        selectedTabIndex = pagerState.currentPage,
+        backgroundColor = Color.Cyan,
+        contentColor = Color.Black,
+        indicator = { tabPositions ->
+            TabRowDefaults.Indicator(
+                Modifier.pagerTabIndicatorOffset(pagerState, tabPositions)
+            )
+        }
+    ) {
+        tabs.forEachIndexed{ index, tab ->
+
+            LeadingIconTab(
+                icon = { Icon(painter = painterResource(id = tab.icon), contentDescription = "") },
+                text = { Text(text = tab.title) },
+                selected = pagerState.currentPage == index,
+                onClick = {
+                    scope.launch {
+                        pagerState.animateScrollToPage(index)
+                    }
+                },
+            )
+        }
+    }
+}
+
+@ExperimentalPagerApi
+@Composable
+fun TabsContent(tabs: List<TabItem>, pagerState: PagerState){
+    HorizontalPager(state = pagerState) { page ->
+        tabs[page].screen()
+    }
+}
+
+@Composable
+fun HomeScreen(){
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Home",
+            fontWeight = FontWeight.Bold,
+            color = Color.Black,
+            fontSize = 30.sp
+        )
+    }
+
+}
+
+@Composable
+fun ProfileScreen(){
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Profile",
+            fontWeight = FontWeight.Bold,
+            color = Color.Black,
+            fontSize = 30.sp
+        )
+    }
+
+}
+
+@Composable
+fun SettingsScreen(){
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Settings",
+            fontWeight = FontWeight.Bold,
+            color = Color.Black,
+            fontSize = 30.sp
+        )
+    }
+
+}
